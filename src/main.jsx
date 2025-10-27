6c3d054e56c21309e6a897888464f778c24aa653
dhinakaran-Y <dhinakaran.y.dhina14@gmail.com>
2025-10-27 10:01:18 +0530

    init-6

--- src/main.jsx
+++ src/main.jsx
@@ -7,21 +7,21 @@ import StoryPage from './assets/pages/StoryPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/Instagram-clone-app/",
     element: <MainLayout />,
     errorElement: <ErrorPage />,
 
     children: [
       {
-        path: "/",
+        path: "/Instagram-clone-app/",
         element: <HomePage />,
       },
     ],
   },
   {
-    path: "/story/:id",
-    element:<StoryPage/>
-  }
+    path: "/Instagram-clone-app/story/:id",
+    element: <StoryPage />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
